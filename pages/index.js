26e68a90749fd3039fcfd754c653fbe29161afd1
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2025-08-11 10:24:56 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Juliete, eu amo você. Se você me ama dê uma risadinha</h1>
+    return <h1>Juliete, eu amo você. Se você me ama dê uma risadinha!</h1>
 }
 
 export default Home; //export default centraliza em uma function, os componentes (funções) que serão renderizados na página
