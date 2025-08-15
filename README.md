1018f51aca345733910362a54fe39cb2c93aa4da
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2025-08-15 09:55:21 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para treinamento do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Juliete, eu amo você. Se você me ama dê uma risadinha!</h1>
+function Home() {
+  return <h1>Juliete, eu amo você. Se você me ama dê uma risadinha!</h1>;
+}
+
+function teste() {
+
+}
+function teste2() {
+  console.log("indentação erradaasdfaa")
+  console.log("eu sou um teste")
 }
 
 export default Home; //export default centraliza em uma function, os componentes (funções) que serão renderizados na página
